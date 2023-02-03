d1e5b09f5c7b2ba35694e62a796e2bc2b296ce45
yuri <yurialec@hotmail.com>
2023-02-03 16:11:07 -0300

    criadas classes e relacionamento das entidades

---
+++ src/main/java/com/devsuperior/userdept/entities/Departament.java
@@ -0,0 +1,29 @@
+package com.devsuperior.userdept.entities;
+
+public class Departament {
+
+	private Long id;
+	private String name;
+	
+	public Departament() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ src/main/java/com/devsuperior/userdept/entities/User.java
@@ -0,0 +1,48 @@
+package com.devsuperior.userdept.entities;
+
+public class User {
+
+	private Long id;
+	private String name;
+	private String email;	
+	
+	private Departament departament;
+	
+	public User() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Departament getDepartament() {
+		return departament;
+	}
+
+	public void setDepartament(Departament departament) {
+		this.departament = departament;
+	}
+	
+	
+}
