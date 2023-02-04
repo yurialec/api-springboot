58e370eb95527cf588e990aa78f02b7ce37c91c8
yuri <yurialec@hotmail.com>
2023-02-04 14:06:04 -0300

    Rota findAll criada

---
+++ src/main/java/com/devsuperior/userdept/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.devsuperior.userdept.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.userdept.entities.User;
+import com.devsuperior.userdept.repositories.UserRepository;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+	@Autowired
+	private UserRepository repository;
+
+	@GetMapping
+	public List<User> findAll() {
+		List<User> result = repository.findAll();
+		return result;
+	}
+}
--- src/main/java/com/devsuperior/userdept/entities/User.java
+++ src/main/java/com/devsuperior/userdept/entities/User.java
@@ -11,21 +11,20 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_user")
 
-
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	
+
 	private Long id;
 	private String name;
 	private String email;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "department_id")
 	private Department department;
-	
+
 	public User() {
-		
+
 	}
 
 	public Long getId() {
@@ -59,6 +58,5 @@ public class User {
 	public void setDepartament(Department department) {
 		this.department = department;
 	}
-	
-	
+
 }
