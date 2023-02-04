6c30eb91e6debc62fe5795d30b15974fc60763b2
yuri <yurialec@hotmail.com>
2023-02-04 18:39:30 -0300

    Crud

--- src/main/java/com/devsuperior/userdept/controllers/UserController.java
+++ src/main/java/com/devsuperior/userdept/controllers/UserController.java
@@ -3,7 +3,12 @@ package com.devsuperior.userdept.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +24,32 @@ public class UserController {
 
 	@GetMapping
 	public List<User> findAll() {
+		List<User> result = repository.findAll();
+		return result;
+	}
+
+	@GetMapping(value = "/{id}")
+	public User findById(@PathVariable Long id) {
+		User result = repository.findById(id).get();
+		return result;
+	}
+
+	@PostMapping
+	public User insert(@RequestBody User user) {
+		User result = repository.save(user);
+		return result;
+	}
+
+	@PutMapping(value = "/{id}")
+	public User update(@PathVariable Long id, @RequestBody User user) {
+		user.setId(id);
+		User result = repository.save(user);
+		return result;
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public List<User> deleteById(@PathVariable Long id) {
+		repository.deleteById(id);
 		List<User> result = repository.findAll();
 		return result;
 	}
